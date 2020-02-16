41967b264c3a1157ba5a84209cfd6a2172078f47
too-03 <o3.426.61_family@i.softbank.jp>
2020-02-16 15:30:29 +0900

    add_message_sending_function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,10 +24,11 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
+    
   end
 
   private
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,19 @@
 class Group < ApplicationRecord
+  has_many :messages
   has_many :group_users
   has_many :users, through: :group_users
+
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.body?
+        last_message.body
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージがありません'
+    end
+  end
 end
